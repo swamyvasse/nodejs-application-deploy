bbf11e4f7cf2219ca321e2dd32e71c967bc0d6f7
swamy vasse <swamyvasse@gmail.com>
2024-11-09 11:53:43 +0530

    add .env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+require("dotenv").config
 
 
 app.get('/api/get',(req,res) => {
@@ -13,11 +14,11 @@ app.get('/api/get_user_details',(req,res) => {
             name:"swamy",
             age:25,
             contact:9182167868
-        }
+        },env:process.env.NAME
     })
 });
 
 
-app.listen(8000,() => {
+app.listen(process.env.PORT,() => {
     console.log("listening to 8000");
 })
