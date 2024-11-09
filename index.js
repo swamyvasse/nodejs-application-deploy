12535745bcc87f499fc4f673bff387b6fdd09231
swamy vasse <swamyvasse@gmail.com>
2024-11-09 11:33:36 +0530

    add contact detatils

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
+
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"swamy",
+            age:25,
+            contact:9182167868
+        }
+    })
+});
+
+
 app.listen(8000,() => {
     console.log("listening to 8000");
 })
